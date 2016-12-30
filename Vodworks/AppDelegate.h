c994100b05dd0be7a35ba0b0a7c08f47e51f8062
Ehsan Siddique <ehsans@tracersystems.co.uk>
2016-12-30 12:39:42 +0500

    initial commit

---
+++ Vodworks/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Vodworks
+//
+//  Created by Ehsan Saddique on 30/12/2016.
+//  Copyright © 2016 Ehsan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Vodworks/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Vodworks
+//
+//  Created by Ehsan Saddique on 30/12/2016.
+//  Copyright © 2016 Ehsan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
